b475e9c770e4ef8aeca691471172136cdc9f5fa7
Naveen S <naveen2aug@gmail.com>
2017-10-28 16:35:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,96 @@
 <?php
-header(“Location: https://www.kruptos.club”);
-$file=“iplog.txt”;
-$f=fopen($file,‘a’); 
-fwrite($f,“————————-“.”n”);
-fwrite($f,“IP Address:”.$_SERVER[‘REMOTE_ADDR’].”n”); 
-fwrite($f,“User Agemt:”.$_SERVER[‘HTTP_USER_AGENT’].”n”); 
-fwrite($f,“Host Name:”.php_uname(‘n’).”n”); 
-fwrite($f,“Operating System:”.php_uname(‘v’).”(“.php_uname(‘s’).”)”.“n”); 
-fwrite($f, '['.$timestamp.']: '.$ipaddress.' '.$webpage.' '.$browser. "\n<br><br>");
-fclose($f);
+$file = "iplog.txt";
+$returnurl = "kruptos.club";
+function getRealIpAddr() {
+	if (!empty($_SERVER['HTTP_CLIENT_IP']))
+	{
+		$ip=$_SERVER['HTTP_CLIENT_IP'];
+	}
+	elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+	{
+		$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+	}
+	else
+	{
+		$ip=$_SERVER['REMOTE_ADDR'];
+	}
+	return $ip;
+}
+function getBrowser() 
+{ 
+    $u_agent = $_SERVER['HTTP_USER_AGENT']; 
+    $bname = 'Unknown';
+    $platform = 'Unknown';
+    $version= "";
+    if (preg_match('/linux/i', $u_agent)) {
+        $platform = 'linux';
+    }
+    elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+        $platform = 'mac';
+    }
+    elseif (preg_match('/windows|win32/i', $u_agent)) {
+        $platform = 'windows';
+    }
+    if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) 
+    { 
+        $bname = 'Internet Explorer'; 
+        $ub = "MSIE"; 
+    } 
+    elseif(preg_match('/Firefox/i',$u_agent)) 
+    { 
+        $bname = 'Mozilla Firefox'; 
+        $ub = "Firefox"; 
+    } 
+    elseif(preg_match('/Chrome/i',$u_agent)) 
+    { 
+        $bname = 'Google Chrome'; 
+        $ub = "Chrome"; 
+    } 
+    elseif(preg_match('/Safari/i',$u_agent)) 
+    { 
+        $bname = 'Apple Safari'; 
+        $ub = "Safari"; 
+    } 
+    elseif(preg_match('/Opera/i',$u_agent)) 
+    { 
+        $bname = 'Opera'; 
+        $ub = "Opera"; 
+    } 
+    elseif(preg_match('/Netscape/i',$u_agent)) 
+    { 
+        $bname = 'Netscape'; 
+        $ub = "Netscape"; 
+    } 
+    
+    $known = array('Version', $ub, 'other');
+    $pattern = '#(?<browser>' . join('|', $known) .
+    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+    if (!preg_match_all($pattern, $u_agent, $matches)) {
+    }
+    $i = count($matches['browser']);
+    if ($i != 1) {
+        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+            $version= $matches['version'][0];
+        }
+        else {
+            $version= $matches['version'][1];
+        }
+    }
+    else {
+        $version= $matches['version'][0];
+    }
+    if ($version==null || $version=="") {$version="?";}
+    return array(
+        'userAgent' => $u_agent,
+        'name'      => $bname,
+        'version'   => $version,
+        'platform'  => $platform,
+        'pattern'    => $pattern
+    );
+} 
+$ip = getRealIpAddr();
+$ua = getBrowser();
+$logdetails=date("d-m-Y H:i:s")."---{$ip}---{$ua['name']} {$ua['version']}---{$ua['platform']}\n";
+file_put_contents($file, $logdetails, FILE_APPEND | LOCK_EX);
+header("Location: {$returnurl}");
 ?>
